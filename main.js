fff90fca3cea41addbe3a2bd041befbf1e24af95
Marie Fedinová <marie.fedinova@gmail.com>
2021-03-28 12:40:44 +0200

    Panáček se pohybuje podle šipek.

--- main.js
+++ main.js
@@ -5,5 +5,23 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 }
 */
 
+let panacek = document.querySelector('#panacek');
 
-// sem začni psát svůj program
+function move(event) {
+	let position = panacek.getBoundingClientRect();
+	let key = event.keyCode;
+	let maxWidth = window.innerWidth;
+	if (key == '38') {
+        panacek.style.top = position.y - 15 + 'px';
+    }
+    else if (key == '40') {
+        panacek.style.top = position.y + 15 + 'px';
+    }
+    else if (key == '37' && position.x>=15 && position.x <= maxWidth) {
+		panacek.style.left = position.x - 15 + 'px';
+    }
+    else if (key == '39' && position.x>=0 && position.x <= (maxWidth - 15)) {
+		panacek.style.left = position.x + 15 + 'px';
+    }
+	console.log(maxWidth);
+}
