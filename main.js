a69700a2a519288ae8d986bf82a8310ed2976837
Marie Fedinová <marie.fedinova@gmail.com>
2021-04-02 17:44:31 +0200

    Přidání obrázků dle pohybu panáčka, sběr mince a její objevení na náhodném místě.

--- main.js
+++ main.js
@@ -5,23 +5,80 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 }
 */
 
-let panacek = document.querySelector('#panacek');
+let panacek, panacekX, panacekY, panacekSirka, panacekVyska;
+let mince, minceX, minceY, minceSirka, minceVyska;
+
+
 
 function move(event) {
 	let position = panacek.getBoundingClientRect();
 	let key = event.keyCode;
 	let maxWidth = window.innerWidth;
-	if (key == '38') {
-        panacek.style.top = position.y - 15 + 'px';
+	let maxHeight = window.innerHeight;
+	if (key == '38' && panacekY>=15 && panacekY<=maxHeight) {
+        panacekY = panacekY - 15;
+        panacek.src = 'obrazky/panacek-nahoru.png'
     }
-    else if (key == '40') {
-        panacek.style.top = position.y + 15 + 'px';
+    else if (key == '40' && panacekY>=0 && panacekY<=(maxHeight - panacekVyska - 15)) {
+        panacekY = panacekY + 15;
+        panacek.src = 'obrazky/panacek.png'
     }
-    else if (key == '37' && position.x>=15 && position.x <= maxWidth) {
-		panacek.style.left = position.x - 15 + 'px';
+    else if (key == '37' && panacekX>=15 && panacekX <= maxWidth) {
+		panacekX = panacekX - 15;
+    panacek.src = 'obrazky/panacek-vlevo.png'
     }
-    else if (key == '39' && position.x>=0 && position.x <= (maxWidth - 15)) {
-		panacek.style.left = position.x + 15 + 'px';
+    else if (key == '39' && panacekX>=0 && panacekX <= (maxWidth - panacekSirka - 15)) {
+		panacekX = panacekX + 15;
+    panacek.src = 'obrazky/panacek-vpravo.png'
     }
-	console.log(maxWidth);
+  
+	umistiPanacka();
+  testCollision();  
+}
+
+function onPageLoad() {
+  panacek = document.querySelector('#panacek');
+  mince = document.querySelector('#mince');
+
+	// zjistíme šířku a výšku panáčka
+  panacekSirka = panacek.width;
+  panacekVyska = panacek.height;
+
+	// a umístíme panáčka do středu okna
+  panacekX = window.innerWidth / 2 - panacekSirka / 2;
+  panacekY = window.innerHeight / 2 - panacekVyska / 2;
+
+	// pozice panáčka
+  umistiPanacka();
+
+	// zjistíme šířku a výšku mince
+  minceSirka = mince.width;
+  minceVyska = mince.height;
+
+	// a vygenerujeme první minci na náhodné pozici
+  newCoin();
+
+}
+
+// funkce, která umístí panáčka na jeho souřadnice
+// tj. nastaví jeho style.left a style.top na hodnoty panacekX, panacekY
+function umistiPanacka() {
+	panacek.style.left = panacekX  + 'px';
+	panacek.style.top = panacekY  + 'px';
+}
+
+// funkce pro nahodné vygenerování nové pozice mince
+// a umístění mince na tyto souřadnice
+function newCoin() {
+	minceX = Math.random() * (window.innerWidth - minceSirka);
+  minceY = Math.random() * (window.innerHeight - minceVyska);
+  mince.style.left = minceX + 'px';
+  mince.style.top = minceY + 'px';
+}
+
+// fuknce pro otestování kolize panáčka s mincí
+function testCollision() {
+	if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || panacekY + panacekVyska < minceY || minceY + minceVyska < panacekY)) {
+    newCoin();
+  }
 }
